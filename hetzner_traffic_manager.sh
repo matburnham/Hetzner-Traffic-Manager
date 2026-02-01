1dc1fb6a3b22bca6e13ce1f6902066d8a7d89502
Mat Burnham <matburnham@gmail.com>
2026-02-01 21:51:17 +0000

    Work around Hetzner bug

--- hetzner_traffic_manager.sh
+++ hetzner_traffic_manager.sh
@@ -6,6 +6,10 @@
 LIMIT_TB_OFFSET="${LIMIT_TB_OFFSET:--1}"
 LIMIT_GB_OFFSET="${LIMIT_GB_OFFSET:-0}"
 
+echo Hetzner Traffic Manager
+echo Server ID: $SERVER_ID
+echo Limit offset: $LIMIT_TB_OFFSET TB + $LIMIT_GB_OFFSET GB
+
 # Calculation basis (1 TB = 1024^4 Bytes, 1 GB = 1024^3 Bytes)
 LIMIT_BYTES_OFFSET=$((LIMIT_TB_OFFSET * 1024**4 + LIMIT_GB_OFFSET * 1024**3))
 
@@ -45,6 +49,14 @@ SERVERS_JSON=$(get_servers_json)
 STATUS=$(get_status)
 TRAFFIC_BYTES=$(get_traffic)
 TRAFFIC_BYTES_LIMIT=$(get_traffic_limit)
+
+# Hetzner API has a bug which shows "server.included_traffic" as "0" for the first few days of  the month
+# See https://github.com/JanisPlayer/Hetzner-Traffic-Manager/issues/1#issuecomment-3832043583
+if [[ "$TRAFFIC_BYTES_LIMIT" == "0" ]]; then
+    echo "server.included_traffic response is 0 (Hetzner API bug). Falling back to 20 TB."
+    TRAFFIC_BYTES_LIMIT=$((20 * 1024**4))
+fi
+
 LIMIT_BYTES=$((TRAFFIC_BYTES_LIMIT+LIMIT_BYTES_OFFSET))
 
 echo "Current outgoing traffic: $((TRAFFIC_BYTES / 1024**3)) GB"
@@ -53,7 +65,7 @@ echo "Outgoing traffic limit: $((TRAFFIC_BYTES_LIMIT / 1024**4)) TB (including o
 # Check server status and if traffic exceeds the limit
 if [[ "$STATUS" == "running" ]]; then
   if ((TRAFFIC_BYTES >= LIMIT_BYTES)); then
-      echo "Traffic limit of $LIMIT_TB TB reached! The server will be shut down."
+      echo "Traffic limit of $((LIMIT_BYTES / 1024**4)) TB reached! The server will be shut down."
       shutdown_server
   else
       echo "Traffic limit not reached yet. Current traffic: $TRAFFIC_BYTES Bytes of $LIMIT_BYTES Bytes"
